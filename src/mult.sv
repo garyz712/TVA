b58c268696c8706ad527f3030337a4e9326d2a10
Tianwei Liu <tianwei.liu@outlook.com>
2025-04-30 11:19:29 -0700

    correct mult top level

--- src/mult.sv
+++ src/mult.sv
@@ -1,52 +1,42 @@
 //------------------------------------------------------------------------------
 // mult.sv
 //
-// This is one stage of an 4 stage (5 depending on how you look at it)
-// pipelined multiplier that multiplies 2 16-bit integers and returns
-// the low 16 bits of the result.  This is not an ideal multiplier but
-// is sufficient to allow a faster clock period than straight *
+// This is an 4 stage (5 depending on how you look at it) pipelined 
+// multiplier that multiplies 2 16-bit integers and returns the low 64 bits 
+// of the result.  This is not an ideal multiplier but is sufficient to 
+// allow a faster clock period than straight *
+// This module instantiates 4 pipeline stages as an array of submodules.
 //
 // Feb. 10 2023    Tianwei Liu    Initial version
 // Apr. 30 2025    Tianwei Liu    Configure to 16 bit
 //------------------------------------------------------------------------------
 
 `define STAGE 4
-parameter bit [(16/`STAGE)-1:0] ZERO = 'b0;
 
-module mult_stage(
-                    input clock, reset, start,
-                    input [15:0] product_in, mplier_in, mcand_in,
+module mult(
+                input clock, reset,
+                input [15:0] mcand, mplier,
+                input start,
 
-                    output logic done,
-                    output logic [15:0] product_out, mplier_out, mcand_out
-                );
+                output [15:0] product,
+                output done
+            );
 
-
-
-    logic [15:0] prod_in_reg, partial_prod_reg;
-    logic [15:0] partial_product, next_mplier, next_mcand;
-
-    assign product_out = prod_in_reg + partial_prod_reg;
-
-    assign partial_product = mplier_in[(16/`STAGE)-1:0] * mcand_in;
-
-    assign next_mplier = {ZERO,mplier_in[15:(16/`STAGE)]};
-    assign next_mcand = {mcand_in[16-(16/`STAGE)-1:0],ZERO};
-
-    //synopsys sync_set_reset "reset"
-    always_ff @(posedge clock) begin
-        prod_in_reg      <= product_in;
-        partial_prod_reg <= partial_product;
-        mplier_out       <= next_mplier;
-        mcand_out        <= next_mcand;
-    end
-
-    // synopsys sync_set_reset "reset"
-    always_ff @(posedge clock) begin
-        if(reset)
-            done <= 1'b0;
-        else
-            done <= start;
-    end
+  logic [15:0] mcand_out, mplier_out;
+  logic [((`STAGE-1)*16)-1:0] internal_products, internal_mcands, internal_mpliers;
+  logic [(`STAGE-2):0] internal_dones;
+  
+    mult_stage mstage [(`STAGE-1):0]  (
+        .clock(clock),
+        .reset(reset),
+        .product_in({internal_products,16'h0}),
+        .mplier_in({internal_mpliers,mplier}),
+        .mcand_in({internal_mcands,mcand}),
+        .start({internal_dones,start}),
+        .product_out({product,internal_products}),
+        .mplier_out({mplier_out,internal_mpliers}),
+        .mcand_out({mcand_out,internal_mcands}),
+        .done({done,internal_dones})
+    );
 
 endmodule
